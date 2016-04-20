49b0649f0becf9f08516b1f34ab1d5f7d3e620e8
Connor Boesch <Connor_Boesch@yahoo.com>
2016-04-20 01:18:09 -0500

    Initial commit to repo.

---
+++ ChemicalAdapter/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChemicalAdapter {
+	/// <summary>
+	/// MainApp startup class
+	/// </summary>
+	class MainApp {
+		/// <summary>
+		/// Defines the entry point of the application.
+		/// </summary>
+		static void Main() {
+			//Non-adapted chemical compound
+			Compound unknown = new Compound("Unknown");
+			unknown.Display();
+			//Adapted chemical compounds
+			Compound water = new RichCompound("Water");
+			Compound benzene = new RichCompound("Benzene");
+			Compound ethanol = new RichCompound("Ethanol");
+			water.Display();
+			benzene.Display();
+			ethanol.Display();
+			//Wait for user.
+			Console.ReadKey();
+		}
+	}
+}
